9aa33b279a638461b06e1288e96c9a470e0fc052
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2024-10-17 19:02:01 +0200

    Pas d'update de l'avatar en dev

--- events/client/ready.js
+++ events/client/ready.js
@@ -6,8 +6,11 @@ module.exports = async (client) => {
 
         await client.user.setUsername(username);
 
-        const newAvatar = fs.readFileSync('./images/Orca.png');
-        await client.user.setAvatar(newAvatar);
+        if(client.args[0] != "dev"){
+            const newAvatar = fs.readFileSync('./images/Orca.png');
+            await client.user.setAvatar(newAvatar);
+        }
+    
     } catch (error){
         console.error('Error updating bot name and avatar:', error);
     }
