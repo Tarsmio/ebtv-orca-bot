37deee9c63280673bbba6d6a230e2db1fe082061
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2024-10-24 19:23:43 +0200

    ???

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { loadCommands, loadEvents } = require('./utils/loaders');
-const { getNbStage, getParticipants } = require('./utils/toornamentUtils');
+const { getNbStage, getParticipants, getStreamIds, getStageIds } = require('./utils/toornamentUtils');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -10,12 +10,6 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 client.args = process.argv.slice(2)
 
-async function test() {
-    const participant = await getParticipants(50, 75)
-    participant.data.forEach(element => {
-        console.log(`"${element.name}" : "${element.id}",`)
-    })
-}
 
 loadCommands(client)
 loadEvents(client)
