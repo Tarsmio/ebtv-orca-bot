47ea13f850cdb294966f8aba918469573cf6ace9
Tristan Chefai <tristan.chefai@etu.univ-tlse2.fr>
2024-10-30 23:10:01 +0100

    Ajout de la commande bulkdelete

---
+++ commands/Outils/bulkdelete.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { STAFF_EBTV, TO, ADMIN } = require('../../utils/roleEnum');
+
+module.exports.execute = async (interaction) => {
+    interaction.deferReply()
+
+    const channel = interaction.channel
+    const numberToDelete = interaction.options.getNumber("nombre_messages")
+
+    channel.bulkDelete(numberToDelete).then(rep => {
+        interaction.editReply({
+            content: `${rep.size} messages viennent d'être supprimer`,
+            ephemeral: true
+        })
+    }).catch(err => {
+        interaction.editReply({
+            content: `Une erreur est survenue lors de la suppression des messages : ${err}`,
+            ephemeral: true
+        })
+    })
+}
+
+module.exports.info = {
+    name: "bulkdelete",
+    description: 'Supprime plusieurs messages en même temps',
+    rolePermission: [ADMIN, STAFF_EBTV, TO],
+    userPersmission: [],
+    helpReportType: 1,
+    category : "outils"
+}
+
+module.exports.dataSlash = new SlashCommandBuilder()
+    .setName(this.info.name)
+    .setDescription(this.info.description)
+    .addNumberOption(opt =>
+        opt.setName("nombre_messages")
+            .setDescription("le nombre de message a delete")
+            .setMinValue(1)
+            .setMaxValue(50)
+            .setRequired(true)
+    )
