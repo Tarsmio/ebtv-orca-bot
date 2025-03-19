56d48bec099ceb169a5359d1a2661492a083cb53
Tarsmio <tarsmio@gmail.com>
2025-03-19 19:15:10 +0100

    Notif planif match

--- commands/toornament/planif.js
+++ commands/toornament/planif.js
@@ -4,11 +4,13 @@ const { checkUserPermissions } = require("../../utils/logging/logger");
 const { parseAndFormatDate } = require("../../utils/planification/date");
 const { STAFF_EBTV, TO } = require('../../utils/roleEnum');
 const { randomInt, getDayOfWeekWithDate } = require('../../utils/utilityTools');
+const { fetchUniqueGroup } = require('../../utils/groupUtils');
+const { getCategoryCastMatch } = require('../../utils/castChannel/castChannelUtils');
 
-function isEster(channelId){
+function isEster(channelId) {
     easterNumber = randomInt(1, 10)
 
-    if((channelId == process.env.PRIVATE_CMD_CHANNEL) && (easterNumber == 6)){
+    if ((channelId == process.env.PRIVATE_CMD_CHANNEL) && (easterNumber == 6)) {
         return true
     } else {
         return false
@@ -16,30 +18,46 @@ function isEster(channelId){
 }
 
 module.exports.execute = async (interaction) => {
-    
+
     try {
         await interaction.deferReply();
 
         const formattedDate = parseAndFormatDate(interaction.options.getString("date"), interaction.options.getString("heure"));
 
+        let team1 = interaction.options.getRole("équipe1").name
+        let team2 = interaction.options.getRole("équipe2").name
+
         let match = await fetchUniqueMatch(
-            interaction.options.getRole("équipe1").name,
-            interaction.options.getRole("équipe2").name
+            team1,
+            team2
         );
 
-        if(match == null){
+        if (match == null) {
             return interaction.editReply({
-                content : "Match introuvable !"
+                content: "Match introuvable !"
             })
         }
 
 
-        if(await setPlanif(formattedDate, match[0].id)){
-            if(isEster(interaction.channel.id)){
+        if (await setPlanif(formattedDate, match[0].id)) {
+            const divisionName = await fetchUniqueGroup(match[0]?.group_id);
+
+            const divisionPattern = divisionName.name.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+            const divCat = await getCategoryCastMatch(interaction.guild, divisionPattern);
+
+            let forumPattern = /^div-[0-9]{1,2}-stats$/
+
+            let statChannel = divCat.children.cache.find(c => forumPattern.test(c.name))
+
+            let statMatchChannel = statChannel.threads.cache.find(t => t.name == `${team1} contre ${team2}` || t.name == `${team2} contre ${team1}`)
+
+            await statMatchChannel.send(`Le match est prevu pour le ${getDayOfWeekWithDate(formattedDate.substring(0, 10))} à ${formattedDate.substring(11, 16)}.`)
+            if (isEster(interaction.channel.id)) {
                 return await interaction.editReply({
                     content: `Eh ben putain t'as bien pris ton temps à planif ducon hein ? Tout ça pour le planifier pour le ${getDayOfWeekWithDate(formattedDate.substring(0, 10))} à ${formattedDate.substring(11, 16)} ?! Non mais quelle plaie je te jure... Et t'as pensé au caster ? Non évidemment que non ! Peuchère...\n\nBisous`
                 })
-            } else if(interaction.user.id == "362246536286961665"){
+            } else if (interaction.user.id == "362246536286961665") {
                 return await interaction.editReply({
                     content: `Mon tres cher Gaby t'as de la chance que je sois obliger de repondre a ta demande ||:middle_finger:||`
                 })
@@ -65,7 +83,7 @@ module.exports.info = {
     rolePermission: [STAFF_EBTV, TO],
     userPersmission: [],
     helpReportType: 1,
-    category : "toornament",
+    category: "toornament",
     active: true,
     isPublic: true
 }
